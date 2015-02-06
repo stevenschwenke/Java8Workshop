ce5d03f964a84ff29147232f731557a2209f770a
steven <steven@stevenschwenke.de>
2015-02-06 15:44:08 +0100

    background information about default methods

--- src/test/java/C_02_Default_Methods.java
+++ src/test/java/C_02_Default_Methods.java
@@ -38,12 +38,14 @@ public class C_02_Default_Methods {
     // Let's take a break here.
     //
     // Default methods seem to be a really nice add-on to the language. However, they have been introduced mainly
-    // to make the new Stream-API possible. The common developer may use them, but there are several blog posts
-    // that suggest not to use them as a regular tool. Doing so could result in a mess if your inheritance hierarchy is
-    // more complex. Imagine having to "climb up" the inheritance tree to find out what interface overrides which
-    // default method. That's just one additional layer of complexity. To avoid problems here, continue implementing
-    // interfaces. Use default methods only for what they have been introduced: Enhancing old APIs that have to be
-    // downwards compatible.
+    // to make the new Stream-API possible. The interface Iterable got the new method forEach(). This new method has
+    // a default implementation. Otherwise, all classes implementing Iterable would have had to be rewritten with an
+    // implementation of this new method.
+    // The common developer may use default methods, but there are several blog posts  that suggest not to use them
+    // as a regular tool. Doing so could result in a mess if your inheritance hierarchy is more complex. Imagine
+    // having to "climb up" the inheritance tree to find out what interface overrides which default method. That's
+    // just one additional layer of complexity. To avoid problems here, continue implementing interfaces. Use default
+    // methods only for what they have been introduced: Enhancing old APIs that have to be downwards compatible.
 
     // The above is the reason there are no exercises in this chapter.
 
