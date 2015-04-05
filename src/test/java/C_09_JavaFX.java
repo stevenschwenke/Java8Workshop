1d9825356d66f6c1f08e8b37ff1ecc0716a195e9
steven <steven@stevenschwenke.de>
2015-04-05 11:25:09 +0200

    Show DatePickers support of different chronologies

--- src/test/java/C_09_JavaFX.java
+++ src/test/java/C_09_JavaFX.java
@@ -8,6 +8,14 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.util.StringConverter;
+
+import java.time.LocalDate;
+import java.time.chrono.HijrahChronology;
+import java.time.chrono.JapaneseChronology;
+import java.time.chrono.MinguoChronology;
+import java.time.chrono.ThaiBuddhistChronology;
+import java.time.format.DateTimeFormatter;
 
 /**
  * The official Java GUI framework, JavaFX, also got some new features with Java 8.
@@ -31,8 +39,15 @@ public class C_09_JavaFX extends Application {
     public void start(Stage primaryStage) {
         DatePicker datePicker = new DatePicker();
 
+        datePicker.setShowWeekNumbers(true);
+
+        setFormattingToGermanDateFormat(datePicker);
+
+        setSomeForeignChronologies(datePicker);
+
         Button btn = new Button();
-        btn.setText("Do stuff");
+        btn.setText("getValue()");
+        // Note that output in the console is default formatting, not formatting of the DatePicker!
         btn.setOnAction(event -> System.out.println(datePicker.getValue()));
 
         BorderPane root = new BorderPane();
@@ -44,5 +59,58 @@ public class C_09_JavaFX extends Application {
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+
+    /**
+     * Sets the formatting of a {@link javafx.scene.control.DatePicker} to the german date format "dd.MM.yyyy".
+     *
+     * @param datePicker to set format for
+     */
+    private void setFormattingToGermanDateFormat(DatePicker datePicker) {
+        // Convert date in text field manually:
+        String pattern = "dd.MM.yyyy";
+        datePicker.setPromptText(pattern.toLowerCase());
+
+        datePicker.setConverter(new StringConverter<LocalDate>() {
+            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(pattern);
+
+            @Override
+            public String toString(LocalDate date) {
+                if (date != null) {
+                    return dateFormatter.format(date);
+                } else {
+                    return "";
+                }
+            }
+
+            @Override
+            public LocalDate fromString(String string) {
+                if (string != null && !string.isEmpty()) {
+                    return LocalDate.parse(string, dateFormatter);
+                } else {
+                    return null;
+                }
+            }
+        });
+    }
+
+    /**
+     * Shows the support of {@link javafx.scene.control.DatePicker} for different cultural time systems.
+     *
+     * @param datePicker to set chronology for
+     */
+    private void setSomeForeignChronologies(DatePicker datePicker) {
+        // Japanese calendar.
+//        datePicker.setChronology(JapaneseChronology.INSTANCE);
+
+        // Hijrah calendar.
+//        datePicker.setChronology(HijrahChronology.INSTANCE);
+
+        // Minguo calendar.
+//        datePicker.setChronology(MinguoChronology.INSTANCE);
+
+        // Buddhist calendar.
+//        datePicker.setChronology(ThaiBuddhistChronology.INSTANCE);
     }
 }
