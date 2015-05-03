b5437a9fc91db1d35acefc0e4732c9552a34fcb7
stevenschwenke <steven@stevenschwenke.de>
2015-05-03 14:43:39 +0200

    new chapter about mission control

---
+++ src/test/java/C_11_MissionControl.java
@@ -0,0 +1,34 @@
+/**
+ * MissionControl is the new tool for monitoring Java applications.
+ */
+public class C_11_MissionControl {
+    /*
+        See http://www.oracle.com/technetwork/java/javaseproducts/mission-control/java-mission-control-1998576.html
+
+        MissionControl "JMC" = JMX Console + FlightRecorder "JFR"
+
+        JMX CONSOLE
+        ============
+
+        1. Start MissionControl: [JDK]/bin/jmc.exe
+
+        2. Create launch config with JVM startup flags for flight recorder:
+        -XX:+UnlockCommercialFeatures
+        -XX:+FlightRecorder
+
+        3. Right-Click in JVM Browser -> Start JMX Console
+
+        4. Overview,
+            Triggers,
+            Memory -> Heap Histogram (Click on "Refresh Heap Histogram" two times)
+            Threads -> Deadlock-Detection + "Lock Owner Name"
+            -> TODO Prepare example, maybe a "bad application" with lots of memory usage and deadlock?
+
+        FLIGHT RECORDING
+        =================
+
+        1. (within MissionControl) Right-Click in JVM-Browser -> Start Flight Recording
+        2. Range Slider
+        3. Code -> Hot Methods
+     */
+}
