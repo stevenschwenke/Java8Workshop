aa958ce36cd6456e3e66980f12afba649c8e9a94
stevenschwenke <steven@stevenschwenke.de>
2015-05-12 14:01:50 +0200

    Exercises for funcational interfaces and lambdas and streams

--- src/test/java/C_01_FunctionInterfacesAndLambdas_Exercises.java
+++ src/test/java/C_01_FunctionInterfacesAndLambdas_Exercises.java
@@ -1,7 +1,9 @@
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 
 /**
  * Exercises for chapter 1: functional interfaces and lambdas.
@@ -9,7 +11,7 @@ import java.util.List;
 public class C_01_FunctionInterfacesAndLambdas_Exercises {
 
     /*
-        Task:
+        Task 1:
         Write the following code using both functional interfaces and lambdas:
         1. Interface "Drivable" for a thing that can drive. Driving things return a String when being asked to drive.
         2. Class "GermanAutobahn" that holds any number of Drivables. There has to be a method printTrafficReport()
@@ -55,5 +57,40 @@ public class C_01_FunctionInterfacesAndLambdas_Exercises {
         germanAutobahn.addDrivable(() -> "Driving blue car");
         germanAutobahn.addDrivable(() -> "Driving red car");
         germanAutobahn.printTrafficReport();
+    }
+
+    /*
+        Task 2:
+        The autobahn shall not be used by blue cars. Blue cars are bad because ... they are blue! Write a Predicate
+        that filters out blue cars from the autobahn!
+     */
+
+
+
+
+
+
+
+
+    // Intentionally left blank
+
+
+
+    @Test
+    public void filterOutBlueCars() {
+
+        List<Drivable> drivables = Arrays.asList(() -> "blue car", () -> "red car", () -> "orange car");
+
+        Predicate<Drivable> predicate = o -> !o.drive().contains("blue");
+
+        evaluate(drivables, predicate);
+    }
+
+    private void evaluate(List<Drivable> list, Predicate<Drivable> predicate) {
+        for (Drivable i : list) {
+            if (predicate.test(i)) {
+                System.out.println(i.drive());
+            }
+        }
     }
 }
---
+++ src/test/java/C_04_Streams_Exercises.java
@@ -0,0 +1,24 @@
+/**
+ * Exercises for chapter 4: Streams.
+ */
+public class C_04_Streams_Exercises {
+
+    /*
+        Task 1: See task 2 from chapter 1. Use streams to simplify the Test.
+     */
+
+
+
+    // Intentionally left blank
+
+
+
+
+
+    /*
+        Solution task 1: Remove evaluate-method and use the following code:
+
+        Stream<Drivable> goodCarsStream = drivables.stream().filter(predicate);
+        goodCarsStream.forEach(o -> System.out.println(o.drive()));
+     */
+}
